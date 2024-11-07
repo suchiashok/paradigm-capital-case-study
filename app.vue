2efac1f36c64f32756a3a4e414c256550f812b9f
suchi ashok <suchiashok.97@gmail.com>
2024-11-07 13:58:45 -0500

    setting components

--- app.vue
+++ app.vue
@@ -1,27 +1,10 @@
 <script setup>
-import { useFetch } from "#app";
-
-const { data: clientData, error } = useFetch(
-  () => `https://paradigmapi.pythonanywhere.com/api/clients/8`
-);
-
-console.log(clientData);
+import ClientComp from './components/clientComp.vue';
+import TradeComp from './components/tradeComp.vue';
 </script>
 
 <template>
-  <div>
-    <h2>Client Data</h2>
-    <div v-if="clientData">
-      <p>Company Name: {{ clientData.company_name }}</p>
-      <p>Company Type: {{ clientData.company_type }}</p>
-      <p>Location: {{ clientData.location }}</p>
-    </div>
+  <ClientComp />
+  <TradeComp />
+</template>
 
-    <div v-else-if="error">
-      <p>Something went wrong: {{ error.message }}</p>
-    </div>
-    <div v-else>
-      <p>Loading</p>
-    </div>
-  </div>
-  </template>
---
+++ components/tradeComp.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useFetch } from "#app";
+
+const { data: tradeData, error } = useFetch(
+  () => `https://paradigmapi.pythonanywhere.com/api/trade/8`
+);
+</script>
+
+<template>
+  <div>
+    <h2>Trade Data</h2>
+    <div v-if="tradeData">
+      <p>Ticker: {{ tradeData.ticker }}</p>
+      <p>Full Ticker: {{ tradeData.full_ticker }}</p>
+      <p>Quantity: {{ tradeData.quantity }}</p>
+    </div>
+
+    <div v-else-if="error">
+      <p>Something went wrong: {{ error.message }}</p>
+    </div>
+    <div v-else>
+      <p>Loading</p>
+    </div>
+  </div>
+</template>
